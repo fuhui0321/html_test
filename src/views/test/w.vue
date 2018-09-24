94e1fa6c523e53291900e749e3919a2d5331a37b
fuhui0321 <fuhui_21@163.com>
2018-09-24 11:13:27 +0800

    3333

---
+++ src/views/test/w.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="animated fadeIn">
+
+    <Row>
+      
+
+        <h3>框架在手，天下我有</h3>
+        <p>好用的框架决定了一个程序员的效率</p>
+
+      
+    </Row>
+    
+  </div>
+</template>
+
+   
+<script>
+
+    export default {
+        data () {
+            return {
+        　　　　　　　　}
+      }
+       
+    }
+</script>
